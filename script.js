2709a714a3a57bb1a4861a2cdaa9cfa24efe7f02
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-01-23 16:50:40 +0200

    html rows removed, put into jQuery instead

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-var scheduleTime = $('.time')
-var saveButton = $('.saveBtn')
-var toDoBox = $('.to-do')
+var scheduleTime = $('.time');
+var saveButton = $('.saveBtn');
+var toDoBox = $('.to-do');
+var hours = $('.hour');
 
 
 // current day displayed at top of calendar
 var today = moment().format("Do MMMM YYYY")
 $("#today").text(today);
+
+
+
 
 // Should rows be inputted by JQuery?
 
@@ -13,26 +17,27 @@ $("#today").text(today);
 var time = moment().hour();
 console.log(time)
 
-var hours = [09, 10, 11, 12, 13, 14, 15, 16, 17];
+// var hours = [09, 10, 11, 12, 13, 14, 15, 16, 17];
 
-// for (let i=0; i<hours.length; i++) {
-//     $(".time").each(function()
-//     )
-// }
+for (var i=9; i< 18; i++) {
+    var section = $('<section>');
+    var hourDiv = $('<div>');
+    var toDo = $('<div>');
+    var savDiv = $('<div>');
+}
 
 // make past hours and future colors different
 function colorTime(){
-    if (hours > time){
+    if (thatTime < time){
         toDoBox.addClass('.past')
     }
-    if (hours === time){
+    if (thatTime === time){
         toDoBox.addClass('.present')
     }
-    if (hours < future){
+    if (thatTime > time){
         toDoBox.addClass('.future')
     }
 }
-
 
 // save input text to local storage
 saveButton.on('click', function() {
