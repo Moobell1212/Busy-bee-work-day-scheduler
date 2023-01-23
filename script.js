811e7547251d7c6579a42fd5d94a03f368c1b0ef
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-01-23 12:51:35 +0200

    initial code for changing time box color added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var scheduleTime = $('.time')
 var saveButton = $('.saveBtn')
+var toDoBox = $('.to-do')
 
-console.log(scheduleTime)
 
 // current day displayed at top of calendar
 var today = moment().format("Do MMMM YYYY")
@@ -10,16 +10,28 @@ $("#today").text(today);
 // Should rows be inputted by JQuery?
 
 // better to put time into the rows??
-// var time = moment();
+var time = moment().hour();
+console.log(time)
 
-// var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var hours = [09, 10, 11, 12, 13, 14, 15, 16, 17];
 
 // for (let i=0; i<hours.length; i++) {
-//     $("#time").text(hours[i])
+//     $(".time").each(function()
+//     )
 // }
 
 // make past hours and future colors different
-
+function colorTime(){
+    if (hours > time){
+        toDoBox.addClass('.past')
+    }
+    if (hours === time){
+        toDoBox.addClass('.present')
+    }
+    if (hours < future){
+        toDoBox.addClass('.future')
+    }
+}
 
 
 // save input text to local storage
