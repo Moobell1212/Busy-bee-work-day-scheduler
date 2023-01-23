cc2a4a60222fb805e78f9cdc00ee925fdb153f55
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-01-23 20:00:10 +0200

    if loops added to change color of hours based on current hour

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-var scheduleTime = $('.time');
-var saveButton = $('.saveBtn');
-var toDoBox = $('.to-do');
-var hours = $('.hour');
-
-
 // current day displayed at top of calendar
 var today = moment().format("Do MMMM YYYY")
-$("#today").text(today);   
+$("#today").text(today);
 
-// better to put time into the rows??
 var time = moment().hour();
 console.log(time);
 
-for (var i=9; i< 18; i++) {
+for (var i = 9; i < 18; i++) {
     var section = $('<section>').addClass('this-hour').attr('id', i);
     var hourDiv = $('<div>').addClass('hour');
     var pTimeEl = $('<p>').addClass('time').text(`${i}:00`);
@@ -20,27 +13,25 @@ for (var i=9; i< 18; i++) {
     var textArea = $('<textarea>');
     var savDiv = $('<div>').addClass('save');
     var saveBtn = $('<button>').addClass('saveBtn').attr('id', i)
-    .on('click', function() {
-        var hourKey = $(this).attr('id');
-        var activity = $(this).parent().siblings('.to-do').children().val();
-        console.log(activity)
-        localStorage.setItem(hourKey, activity)
+        .on('click', function () {
+            var hourKey = $(this).attr('id');
+            var activity = $(this).parent().siblings('.to-do').children().val();
+            console.log(activity)
+            localStorage.setItem(hourKey, activity)
         }
-    )
+        )
     var iClass = $('<i>').addClass('fa fa-save');
     $('.container').append(section.append(hourDiv.append(pTimeEl), toDo.append(textArea), savDiv.append(saveBtn.append(iClass))));
+        if (i < time) {
+            toDo.addClass('past')
+        }
+        else if (i === time) {
+            toDo.addClass('present')
+        }
+        else {
+            toDo.addClass('future')
+        }
+
 }
 
-// make past hours and future colors different
-function colorTime(){
-    if (thatTime < time){
-        toDoBox.addClass('.past')
-    }
-    if (thatTime === time){
-        toDoBox.addClass('.present')
-    }
-    if (thatTime > time){
-        toDoBox.addClass('.future')
-    }
-}
 
